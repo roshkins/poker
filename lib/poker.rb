9ccb45c21dc3470aa77afb28c049472a98f4a646
William Li and Rashi Abramson <student@appacademy.io>
2013-07-26 17:05:55 -0700

    added deck

--- lib/poker.rb
+++ lib/poker.rb
@@ -1,6 +1,53 @@
 class Card
+
+  attr_reader :suit, :value
+
   def initialize(suit, number)
     @suit = suit
-    @number = number
+    @value = number
   end
+
+  VALUES = ["A", "J", "Q", "K"]
+
+  def to_s
+    num_str = \
+    case @value
+    when 1
+      VALUES[0]
+    when 11
+      VALUES[1]
+    when 12
+      VALUES[2]
+    when 13
+      VALUES[3]
+    else
+      @value.to_s
+    end
+    "#{num_str} of #{@suit.to_s.capitalize}"
+  end
+
+end
+
+class Deck
+  attr_reader :cards
+
+  SUITS = [:clubs, :hearts, :spades, :diamonds]
+
+  def initialize
+    @cards = []
+
+    13.times do |value|
+      4.times do |suit|
+        @cards << Card.new(SUITS[suit], value + 1)
+      end
+    end
+  end
+
+  def deal(num_of_cards = 1)
+    return_cards = @cards.sample(num_of_cards)
+    @cards -= return_cards
+    return_cards
+  end
+
+
 end
--- spec/poker_spec.rb
+++ spec/poker_spec.rb
@@ -7,6 +7,32 @@ describe "Game" do
 
 end
 
+describe "Deck" do
+  let(:deck) { Deck.new }
+
+  describe "#new" do
+    it "returns a new deck object" do
+      expect(deck).to be_an_instance_of(Deck)
+    end
+
+    it "returns 52 cards with value in 1..13" do
+      expect(deck.cards.length).to eq(52)
+      deck.cards.each do |card|
+        expect(card.value).to be_between(0,14)
+      end
+    end
+
+  end
+
+  describe "#deal" do
+    it "deck no longer contains the dealt cards" do
+      cards = deck.deal(5)
+      expect(deck.cards).not_to include(cards)
+    end
+  end
+
+end
+
 describe "Card" do
 
   let(:card) do
@@ -26,19 +52,100 @@ describe "Card" do
 
   end
 
-  describe "#number" do
-    it "returns a number" do
-      expect(card.number).to eq(12)
+  describe "#value" do
+    it "returns a value" do
+      expect(card.value).to eq(12)
     end
 
   end
 
   describe "#to_s" do
-    it "prints a suit and a number" do
-      expect(card.to_s).to eq("queen of hearts")
+    it "prints a suit and a value" do
+      expect(card.to_s).to eq("Q of Hearts")
     end
 
   end
 
 end
+
+describe "Hand" do
+
+  let(:deck) do
+    d = double("deck")
+  end
+
+  let(:hand) do
+    hand = Hand.new
+  end
+
+  describe "#straight_flush?" do
+    it "returns true when its a straight flush"
+    it "returns false when its not a straight flush"
+    expect([])
+
+  end
+  describe "#four_of_a_kind?" do
+    it "returns true when its a four of a kind"
+    it "returns false when its not a four of a kind"
+  end
+
+
+  describe "#full_house?" do
+    it "returns true when its a full house"
+    it "returns false when its not a full house"
+  end
+
+  describe "#flush?" do
+    it "returns true when its a flush"
+    it "returns false when its not a flush"
+  end
+
+
+  describe "#straight?" do
+    it "returns true when its a straight"
+    it "returns false when its not a straight"
+  end
+
+  describe "#three_of_a_kind?" do
+    it "returns true when its a three of a kind"
+    it "returns false when its not a three of a kind"
+  end
+
+
+  describe "#two_pair?" do
+    it "returns true when its a two pair"
+    it "returns false when its not a two pair"
+  end
+
+  describe "#one_pair?" do
+    it "returns true when its a one pair"
+    it "returns false when its not a one pair"
+  end
+
+  describe "#high_card?" do
+    it "returns true when its a high card"
+    it "returns false when its not a high card"
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
